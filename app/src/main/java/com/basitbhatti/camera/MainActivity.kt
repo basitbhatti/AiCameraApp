611248c7518512d1a6154a7520dd07d287c2d574
Abdul Basit Bhatti <78906762+basitbhatti@users.noreply.github.com>
2024-11-20 09:56:57 +0500

    Working on business logic.

--- app/src/main/java/com/basitbhatti/camera/MainActivity.kt
+++ app/src/main/java/com/basitbhatti/camera/MainActivity.kt
@@ -2,31 +2,49 @@ package com.basitbhatti.camera
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Camera
 import androidx.compose.material.icons.outlined.Cameraswitch
+import androidx.compose.material.icons.outlined.PhotoAlbum
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.basitbhatti.camera.ui.BottomSheetContent
 import com.basitbhatti.camera.ui.CameraPreview
 import com.basitbhatti.camera.ui.theme.CameraAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -37,8 +55,13 @@ class MainActivity : ComponentActivity() {
             ActivityCompat.requestPermissions(this, PERMISSIONS, 0)
         }
 
+
         setContent {
             CameraAppTheme {
+                val viewModel = viewModel<MainViewModel>()
+                val bitmaps by viewModel.bitmaps.collectAsState()
+
+                val scope = rememberCoroutineScope()
 
                 val scaffoldState = rememberBottomSheetScaffoldState()
 
@@ -51,34 +74,93 @@ class MainActivity : ComponentActivity() {
                 BottomSheetScaffold(
                     scaffoldState = scaffoldState,
                     sheetPeekHeight = 0.dp,
-                    sheetContent = {}
+                    sheetContent = {
+                        BottomSheetContent(bitmaps = bitmaps)
+                    }
                 ) {
 
                     Box(modifier = Modifier.padding(it)) {
                         CameraPreview(modifier = Modifier.fillMaxSize(), controller)
-                    }
 
-                    IconButton(onClick = {
-                        controller.cameraSelector = if (
-                            controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
-                            CameraSelector.DEFAULT_FRONT_CAMERA
-                        } else {
-                            CameraSelector.DEFAULT_BACK_CAMERA
+
+                        IconButton(onClick = {
+                            controller.cameraSelector = if (
+                                controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                                CameraSelector.DEFAULT_FRONT_CAMERA
+                            } else {
+                                CameraSelector.DEFAULT_BACK_CAMERA
+                            }
+
+                        }, modifier = Modifier.offset(16.dp, 16.dp)) {
+                            Icon(
+                                tint = Color.White,
+                                imageVector = Icons.Outlined.Cameraswitch,
+                                contentDescription = "Switch Camera"
+                            )
                         }
 
-                    }, modifier = Modifier.offset(16.dp, 16.dp)) {
-                        Icon(
-                            tint = Color.White,
-                            imageVector = Icons.Outlined.Cameraswitch,
-                            contentDescription = "Switch Camera"
-                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .align(Alignment.BottomCenter),
+                            horizontalArrangement = Arrangement.SpaceAround
+                        ) {
+
+                            IconButton(onClick = {
+                                scope.launch {
+                                    scaffoldState.bottomSheetState.expand()
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.PhotoAlbum,
+                                    contentDescription = "Gallery"
+                                )
+                            }
+
+                            IconButton(onClick = {
+                                takePhoto(controller = controller, viewModel::onPhotoTaken)
+                            }) {
+                                Icon(
+                                    modifier = Modifier.size(60.dp),
+                                    imageVector = Icons.Outlined.Camera,
+                                    contentDescription = "Take Photo"
+                                )
+                            }
+
+
+                        }
+
+
                     }
+
                 }
 
 
             }
         }
     }
+
+    private fun takePhoto(
+        controller : LifecycleCameraController,
+        onPhotoTaken : (Bitmap) -> Unit
+    ){
+        controller.takePicture(
+            ContextCompat.getMainExecutor(applicationContext),
+            object : OnImageCapturedCallback(){
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    super.onCaptureSuccess(image)
+                    onPhotoTaken(image.toBitmap())
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    super.onError(exception)
+                    Log.d("TAGPhoto", "onError: $exception")
+                }
+            }
+        )
+    }
+
+
 
     private fun checkRequiredPermissions(): Boolean {
         return PERMISSIONS.all {
